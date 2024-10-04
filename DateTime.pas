46ab15f3ec7c5accf9acf8e37bbb6006b147742c
Vitaliy Afanasyev <vitafanasyev2000@yandex.ru>
2024-10-04 21:37:03 +0300

    Add convert from minutes to seconds

--- DateTime.pas
+++ DateTime.pas
@@ -2,5 +2,6 @@
   var year := ReadInteger('Введите год: ');
   Assert(year >= 0);
   
-  Print($'Год високосный: {(year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0))}')
+  Print($'Год високосный: {(year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0))}');
+  Print('Количество секунд в минуте: 60');
 end.
