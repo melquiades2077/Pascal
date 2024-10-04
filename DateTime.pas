ebcd0e63b2a980dfb0c901897936425ccb8229b7
Vitaliy Afanasyev <vitafanasyev2000@yandex.ru>
2024-10-04 22:22:00 +0300

    Add what day is closer to New Year

--- DateTime.pas
+++ DateTime.pas
@@ -2,5 +2,19 @@
   var year := ReadInteger('Введите год: ');
   Assert(year >= 0);
   
-  Print($'Год високосный: {(year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0))}')
+  Print($'Год високосный: {(year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0))}');
+  
+  var (firstDay, firstMonth, secondDay, secondMonth) := ReadInteger4('Введите две даты: ');
+  if (12 - firstMonth) < (12 - secondMonth) then
+    Print(1)
+  else if (12 - firstMonth) > (12 - secondMonth) then
+    Print(2)
+  else
+    if (31 - firstDay) < (31 - secondDay) then
+      Print(1)
+    else if (31 - firstDay) > (31 - secondDay) then
+      Print(2)
+    else
+      Print(0)
+  
 end.
