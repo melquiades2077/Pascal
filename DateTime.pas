06be9654c3b1ca665c2213dd6d44ac80e5e0986c
Vitaliy Afanasyev <vitafanasyev2000@yandex.ru>
2024-10-04 22:36:06 +0300

    Add count days in a Year

--- DateTime.pas
+++ DateTime.pas
@@ -2,9 +2,18 @@
   var year := ReadInteger('Введите год: ');
   Assert(year >= 0);
   
-  Print($'Год високосный: {(year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0))}');
+  var leap := (year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0));
   
-  var (firstDay, firstMonth, secondDay, secondMonth) := ReadInteger4('Введите две даты: ');
+  Print($'Год високосный: {leap}{newline}');
+  
+  if leap then
+    PrintLn('В году 366 дней')
+  else
+    PrintLn('В году 365 дней');
+  
+  var (firstDay, firstMonth, secondDay, secondMonth) := ReadInteger4($'{newline}Введите две даты: ');
+  Assert((firstDay in 1..31) and (secondDay in 1..31) and (firstMonth in 1..12) and (secondMonth in 1..12));
+  
   if (12 - firstMonth) < (12 - secondMonth) then
     Print(1)
   else if (12 - firstMonth) > (12 - secondMonth) then
@@ -15,6 +24,5 @@
     else if (31 - firstDay) > (31 - secondDay) then
       Print(2)
     else
-      Print(0)
-  
+      Print(0);
 end.
